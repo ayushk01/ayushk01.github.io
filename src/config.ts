b3b5b6c6ba2608e146150900cfdc1bb7f8782dcd
Ayush Kashyap <ayush.kashyap@docplix.com>
2024-01-13 03:54:45 +0530

    Initial Commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Ayush Kashyap | CS Graduate 2023";
+export const SITE_DESCRIPTION =
+  "Ayush Kashyap: CS Graduate 2023. Explore my journey in computer science, featuring React, Node.js, and mobile app development";
